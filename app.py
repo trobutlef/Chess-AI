f763342855c7e020f3439ac1848e6c821aefa3c6
trobutlef <astuhuamant@gmail.com>
2022-09-22 17:48:23 -0500

    engine and tests

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!usr/bin/env python3
-from flask import Flask, redirect, url_for, render_template 
+from flask import Flask, render_template
+import chess, chess.pgn
 
 app = Flask(__name__)
 
@@ -8,4 +9,4 @@ def index():
   return render_template("index.html")
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(debug=True, port=4000)
---
+++ engine.py
@@ -0,0 +1,16 @@
+import chess
+
+
+class Engine():
+   
+   #constuctor
+   def __init__(self, fen):
+     self.board = chess.Board()
+      self.MAX_DEPTH = 60
+
+
+
+if __name__ == "__main__":
+  #new_engine = Engine(fen)
+
+  print("hello")
